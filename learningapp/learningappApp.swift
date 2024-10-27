e029f56e2bdf70ba980ade3909550aefee5ef8c3
haifn7 <haifa72010@hotmail.com>
2024-10-27 14:53:22 +0300

    Initial Commit

---
+++ learningapp/learningappApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningappApp.swift
+//  learningapp
+//
+//  Created by haifa on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct learningappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
